3c75ff2f4a6663ab3e40dd088807fde8ed7653e5
GreenBayRules <sheshank@outlook.com>
2018-11-04 07:25:03 -0800

    testing if travis ci works
    tests if travis ci is working

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 my_array = ["Donald", "John", "Toby"] # throws error, but it shouldn't
 my_dictionary = {"name": "John", "age": 10} # throws error, but it shouldn't
-boss = ["Donald", "John", "Toby"] # (bosses) should throw error, doesn't
-my_class = ["Math", "LASS", "Science"] # (my_classes) should throw error, doesn't
-heights_of_glass = [10, 3, 8, 9] # (types_of_glasses) should throw error, doesn't
+boss = ["Donald", "John", "Toby"] # (bosses) should throw error, but it doesn't
+my_class = ["Math", "LASS", "Science"] # (my_classes) should throw error, but it doesn't
+heights_of_glass = [10, 3, 8, 9] # (types_of_glasses) should throw error, but it doesn't
 my_information = ["Data", "More data", "More data"] # throws error, but it shouldn't
 my_info = ["Data", "More data", "More data"] # throws error, but it shouldn't
